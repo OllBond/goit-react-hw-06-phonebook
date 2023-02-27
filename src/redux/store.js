58d9e79fe6a043c3fd11f383becc40c634438ff0
Olena <newsroommy@gmail.com>
2023-02-27 07:04:56 +0300

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { persistStore } from 'redux-persist';
-// import rootReducer from './root-reducer';
 import persistedReducer from './root-reducer';
 
 export const store = configureStore({
